6b45a93acfdcee7bea97e0a8fc514cd401b89d3c
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-11-24 17:06:12 +0530

    updated

---
+++ blog-app-redux/src/store/posts/reducer.js
@@ -0,0 +1,14 @@
+
+// Step 2: Let's create the post reducer. 
+// What's Reducer?
+/* 
+  it is a fn-- that takes in two params -- namely state and action.
+  it must return state 
+*/
+const postReducer = (state, action) => {
+  return state;
+}
+
+export default postReducer;
+
+// For step3: refer store/posts/types.js 
