622de9047b2754f5cae92a89095e99f3f45dc9ce
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-11-26 13:48:46 +0530

    removed console logs

--- blog-app-redux/src/store/posts/sagas.js
+++ blog-app-redux/src/store/posts/sagas.js
@@ -7,12 +7,8 @@ const POSTS_API_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 // worker saga -- can connect with rest api and dispatch 
 function* handleFetchRequest(){
-  console.log('============4. Inside handleFetchRequest -- Feature Saga');
-  // 
   const res = yield call(callApi, POSTS_API_URL, 'get')
   if(res.error){
-    console.log('============5. Inside handleFetchRequest/error -- Feature Saga');
-
     // dispatch with error -- FETCH_ERROR
     // use put() from redux-saga
     yield put({
@@ -20,7 +16,6 @@ function* handleFetchRequest(){
       payload: res.error
     })
   }else {
-    console.log('============5. Inside handleFetchRequest/success -- Feature Saga');
     // dispatch with success -- FETCH_SUCCESS
     // use put() from redux-saga
     yield put({
@@ -32,7 +27,6 @@ function* handleFetchRequest(){
 
 // watcher saga  -- will watch specific action type and run the saga
 function* watchFetchRequest(){
-  console.log('============3. Inside watchFetchRequest -- Feature Saga');
   yield takeEvery(FETCH_REQUEST, handleFetchRequest)
 }
 
@@ -91,7 +85,6 @@ function* handleFetchByIdRequest() {
 function* watchFetchByIdRequest() {
   yield takeEvery(FETCH_BY_ID_REQUEST, handleFetchByIdRequest)
 }
-
 
 // feature saga 
 function* postsSaga(){
