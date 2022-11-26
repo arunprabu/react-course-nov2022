b93578757568408a00fea5a6422818c014acee24
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-11-26 13:41:56 +0530

    updated

--- blog-app-redux/src/components/Posts/PostDetails/PostDetails.js
+++ blog-app-redux/src/components/Posts/PostDetails/PostDetails.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { FETCH_BY_ID_REQUEST } from '../../../store/posts/types';
 
-const PostDetails = () => {
+const PostDetails = (props) => {
+
+  useEffect( () => {
+    console.log(props);
+    props.dispatch({
+      type: FETCH_BY_ID_REQUEST
+    });
+  }, []);
+
+  if (props.isLoading){
+    return (<div className='spinner-border text-primary'></div>)
+  }
+
   return (
-    <div>PostDetails</div>
+    <div>
+      <h1>PostDetails</h1>
+
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{props.post.title}</h5>
+          <p className="card-text">{props.post.body}</p>
+          <button type="button" className="btn btn-primary">Edit</button>
+          <button type="button" className="btn btn-danger">Update</button>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
-export default PostDetails
+const mapStateToProps = ( {posts} ) => {
+  console.log(posts);
+  return {
+    isLoading: posts.isLoading,
+    isError: posts.isError,
+    status: posts.status,
+    post: posts.post
+  }
+}
+
+export default connect(mapStateToProps)(PostDetails)
--- blog-app-redux/src/store/posts/sagas.js
+++ blog-app-redux/src/store/posts/sagas.js
@@ -1,7 +1,7 @@
 // This file is for the feature saga. 
 import { all, fork, takeEvery, call, put } from 'redux-saga/effects';
 import { callApi } from '../../utils/callApi';
-import { FETCH_ERROR, FETCH_REQUEST, FETCH_SUCCESS } from './types';
+import { CREATE_ERROR, CREATE_REQUEST, CREATE_SUCCESS, FETCH_BY_ID_ERROR, FETCH_BY_ID_REQUEST, FETCH_BY_ID_SUCCESS, FETCH_ERROR, FETCH_REQUEST, FETCH_SUCCESS } from './types';
 
 const POSTS_API_URL = 'https://jsonplaceholder.typicode.com/posts'
 
@@ -36,12 +36,69 @@ function* watchFetchRequest(){
   yield takeEvery(FETCH_REQUEST, handleFetchRequest)
 }
 
+function* handleCreatePost(data) {
+  try {
+    // to call ajax logic -- use redux-saga's call()
+    const res = yield call(callApi, POSTS_API_URL, 'post', data.payload);
+    if (res.error) {
+      // dispatch with error -- use put() from redux-saga
+      yield put({
+        type: CREATE_ERROR,
+        payload: res.error
+      })
+    } else {
+      // dispatch with success / data -- use put() from redux-saga
+      yield put({
+        type: CREATE_SUCCESS,
+        payload: res
+      })
+    }
+
+  }
+  catch (err) {
+    if (err) {
+      // dispatch with error -- use put() from redux-saga
+      yield put({
+        type: CREATE_ERROR,
+        payload: err
+      })
+    }
+  }
+}
+
+// watcher saga -- watch specific action type and run the saga 
+function* watchCreatePost() {
+  yield takeEvery(CREATE_REQUEST, handleCreatePost);
+}
+
+// worker saga -- can connect with rest api and dispatch 
+function* handleFetchByIdRequest() {
+  const res = yield call(callApi, POSTS_API_URL+'/1', 'get')
+  if (res.error) {
+    yield put({
+      type: FETCH_BY_ID_ERROR,
+      payload: res.error
+    })
+  } else {
+    yield put({
+      type: FETCH_BY_ID_SUCCESS,
+      payload: res
+    })
+  }
+}
+
+// watcher saga  -- will watch specific action type and run the saga
+function* watchFetchByIdRequest() {
+  yield takeEvery(FETCH_BY_ID_REQUEST, handleFetchByIdRequest)
+}
+
+
 // feature saga 
 function* postsSaga(){
-  console.log('============2. Inside postsSaga -- Feature Saga');
-
   yield all([
-    fork(watchFetchRequest)
+    fork(watchFetchRequest),
+    fork(watchCreatePost),
+    fork(watchFetchByIdRequest)
   ])
 }
 
