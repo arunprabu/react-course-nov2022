fea6ae5f9305bfeb2efaaf4aaf50145d3c6170d5
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-11-30 18:29:32 +0530

    updated

--- react-ag-grid-d3-demo/src/components/D3Charts/LineChart.js
+++ react-ag-grid-d3-demo/src/components/D3Charts/LineChart.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from "react";
+import { select, line, curveCardinal } from "d3";
 
 const LineChart = () => {
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
+  const svgRef = useRef();
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y(value => 150 - value)
+      .curve(curveCardinal);
+    svg
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", value => myLine(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+  }, [data]);
+
   return (
-    <div>LineChart</div>
-  )
+    <React.Fragment>
+      <svg className="line-chart-svg" ref={svgRef}></svg>
+      <br />
+    </React.Fragment>
+  );
 }
 
 export default LineChart
