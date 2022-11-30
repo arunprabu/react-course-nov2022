b2d56479cb067ef66108296d6b5f37ab02c48889
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-11-30 17:18:20 +0530

    updated

--- react-ag-grid-d3-demo/src/components/D3Charts/BarChart.js
+++ react-ag-grid-d3-demo/src/components/D3Charts/BarChart.js
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
 
 const BarChart = () => {
+
+  const [data, setData] = useState([
+    {
+      name: 'Virat Kohli',
+      total: 71,
+      color: 'lightgreen'
+    },
+    {
+      name: 'Sachin Tendulkar',
+      total: 100,
+      color: 'red'
+    },
+    {
+      name: 'Ricky Ponting',
+      total: 71,
+      color: 'green'
+    },
+    {
+      name: 'Kumar Sangakkara',
+      total: 63,
+      color: 'blue'
+    },
+    {
+      name: 'Jacques Kallis',
+      total: 62,
+      color: 'pink'
+    }
+  ]);
+  const svgRef = useRef();
+
+  // let extractDataAsList = [];
+  // data.map(item => {
+  //   extractDataAsList = [
+  //     ...extractDataAsList,
+  //     item.total
+  //   ]
+  // })
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const xScale = scaleBand()
+      .domain(data.map((value, index) => index))
+      .range([0, 300])
+      .padding(0.5);
+
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([150, 0]);
+
+    const colorScale = scaleLinear()
+      .domain([60, 65, 70, 75, 100])
+      //.range(["lightgreen", "green", "blue", "pink", "red"])
+      .clamp(true);
+
+    const xAxis = axisBottom(xScale).ticks(data.length);
+
+    svg
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
+
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .style("transform", "scale(1, -1)")
+      .attr("x", (value, index) => {
+        return xScale(index)
+      })
+      .attr("y", -150)
+      .attr("width", xScale.bandwidth())
+      .transition()
+      .attr("fill", (data) => {
+        return data.color;
+      })
+      // .attr("fill", (data, index) => {
+      //   return colorScale.range()[index];
+      // })
+      .attr("height", (data) => {
+        return 150 - yScale(data.total);
+      });
+  }, [data]);
+
   return (
-    <div>BarChart</div>
-  )
+    <React.Fragment>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+    </React.Fragment>
+  );
 }
 
 export default BarChart
--- react-ag-grid-d3-demo/src/components/D3Charts/PieChart.js
+++ react-ag-grid-d3-demo/src/components/D3Charts/PieChart.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { arc, interpolate, pie, scaleOrdinal, schemeSpectral, select } from "d3";
 
 const PieChart = () => {
+  const [data, setData] = useState([
+    {
+      name: "apples",
+      value: 100
+    },
+    {
+      name: "bananas",
+      value: 300
+    },
+    {
+      name: "cherries",
+      value: 250
+    }
+  ]);
+
+  const svgRef = useRef();
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    // arc takes instructions (objects with special properties, like startAngle, endAngle, etc.)
+    // and transforms them into "d" attributes for path elements
+    const arcGenerator = arc()
+      .innerRadius(0)
+      .outerRadius(150);
+
+    // pie will transform data to instructions for arcGenerator
+    const pieGenerator = pie()
+      .value(d => d.value)
+      .sort(null); // makes sure data doesn't get sorted
+
+    // now transform data to instructions for arc()
+    const instructions = pieGenerator(data);
+    console.log(instructions);
+
+    // generate colorScale
+    // https://github.com/d3/d3-scale-chromatic
+    const colorScale = scaleOrdinal(schemeSpectral[data.length]);
+
+    // render slices (instructions)
+    svg
+      .selectAll(".slice")
+      .data(instructions)
+      .join("path")
+      .attr("class", "slice")
+      .attr("fill", (d, i) => colorScale(i))
+      .style(
+        "transform",
+        `translate(${500 / 2}px, ${450 / 2}px)`
+      )
+      .transition()
+      .attrTween("d", function (nextInstruction, index) {
+        // animation when changing data
+        const interpolator = interpolate(this.lastInstruction, nextInstruction);
+        this.lastInstruction = interpolator(1);
+        return function (t) {
+          return arcGenerator(interpolator(t));
+        };
+      });
+
+    svg.selectAll('text')
+      .data(data)
+      .enter()
+      .append("text")
+      .text((d) => `${d.name}: ${d.value}`) // label text
+      .style("fill", "#000")
+      .attr("transform", (data, index) => `translate(${(index * 100) + 25}, 20)`)
+      .transition()
+      .duration(700)
+      .style("font-size", "15px");
+  }, [data]);
+
   return (
-    <div>PieChart</div>
-  )
+    <div style={{ marginBottom: "2rem", height: 1000 }}>
+      <svg width='500' height='400' ref={svgRef}></svg>
+    </div>
+  );
 }
 
 export default PieChart
