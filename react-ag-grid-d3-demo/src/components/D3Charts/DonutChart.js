368bfba1d3f4f76d76a32c213bcf3b37fe99b5ec
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-11-30 15:44:23 +0530

    updated

--- react-ag-grid-d3-demo/src/components/D3Charts/DonutChart.js
+++ react-ag-grid-d3-demo/src/components/D3Charts/DonutChart.js
@@ -4,20 +4,17 @@ import * as d3 from 'd3';
 const DonutChart = () => {
   const donutRef = useRef();
 
-  // have the data for donut chart
+  // have the data for donut chart with colors 
   const browsersMarketShare = [
-    { name: 'Chrome', percent: 65 },
-    { name: 'Safari', percent: 19 },
-    { name: 'Edge', percent: 4 },
-    { name: 'Firefox', percent: 3 },
-    { name: 'Opera', percent: 2.5 },
-    { name: 'Others', percent: 6.5 },
+    { name: 'Chrome', percent: 65, color: '#48cae4' },
+    { name: 'Safari', percent: 19, color: '#a7c957' },
+    { name: 'Edge', percent: 4, color: '#FF4081' },
+    { name: 'Firefox', percent: 3, color: '#FFA000' },
+    { name: 'Opera', percent: 2.5, color: '#ef9a9a' },
+    { name: 'Others', percent: 6.5, color: '#b39ddb' }
   ];
 
-  // identify the colors for all the items 
-  const colors = ['#48cae4', '#a7c957', '#FF4081', '#FFA000', '#ef9a9a', '#b39ddb'];
-
-  useEffect( () => {
+  useEffect(() => {
     console.log(donutRef.current); // the element in which we will add donut chart
     drawChart(donutRef.current);
   }, []);
@@ -32,8 +29,8 @@ const DonutChart = () => {
       .attr('height', 500)
       .attr('viewBox', `0 0 ${boxSize} ${boxSize}`)
       .append('g')
-      .attr('transform', `translate(${boxSize/2}, ${boxSize/2})`) // move the position of the element
-    
+      .attr('transform', `translate(${boxSize / 2}, ${boxSize / 2})`) // move the position of the element
+
     // generate the arcs
     const arcGenerator = d3.arc()
       .cornerRadius(10)
@@ -44,29 +41,28 @@ const DonutChart = () => {
     // generate the pie chart
     const pieGenerator = d3.pie()
       .startAngle(-0.5 * Math.PI)
-      .value( (data) => {
+      .value((data) => {
         return data.percent;
       })
 
     const arcs = svg.selectAll()
       .data(pieGenerator(browsersMarketShare))
       .enter();
-    
+
     // let's add the path tag -- 
     // The <path> SVG element is the generic element to define a shape. 
     // All the basic shapes can be created with a path element.
     arcs.append('path')
       .attr('d', arcGenerator)
-      .style('fill', (data, index) => {
+      .style('fill', ({data}) => {
         console.log(data);
-        console.log(index);
-        return colors[index]
+        return data.color;
       });
 
     // attach the labels
     arcs.append('text')
       .attr('text-anchor', 'middle')
-      .text( ({data}) => {
+      .text(({ data }) => {
         console.log(data);
         return `${data.name}: ${data.percent}%`
       })
@@ -79,7 +75,7 @@ const DonutChart = () => {
     // have the inner circle -- optional
   }
 
-  
+
 
   return (
     <div>
