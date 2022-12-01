bf3944ef6e1535a1035bd90d8ff3c7c33087395c
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-12-01 17:31:26 +0530

    updated

--- react-ag-grid-d3-demo/src/components/D3Charts/PieChart.js
+++ react-ag-grid-d3-demo/src/components/D3Charts/PieChart.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
-import { arc, interpolate, pie, scaleOrdinal, schemeSpectral, select } from "d3";
+import React, { useEffect, useRef, useState } from 'react';
+import { arc, interpolate, pie, scaleOrdinal, schemeSpectral, select } from 'd3';
 
 const PieChart = () => {
   const [data, setData] = useState([
     {
-      name: "apples",
+      name: 'apples',
       value: 100
     },
     {
-      name: "bananas",
+      name: 'bananas',
       value: 300
     },
     {
-      name: "cherries",
+      name: 'cherries',
       value: 250
     }
   ]);
@@ -44,17 +44,17 @@ const PieChart = () => {
 
     // render slices (instructions)
     svg
-      .selectAll(".slice")
+      .selectAll('.slice')
       .data(instructions)
-      .join("path")
-      .attr("class", "slice")
-      .attr("fill", (d, i) => colorScale(i))
+      .join('path')
+      .attr('class', 'slice')
+      .attr('fill', (d, i) => colorScale(i))
       .style(
-        "transform",
+        'transform',
         `translate(${500 / 2}px, ${450 / 2}px)`
       )
       .transition()
-      .attrTween("d", function (nextInstruction, index) {
+      .attrTween('d', function (nextInstruction, index) {
         // animation when changing data
         const interpolator = interpolate(this.lastInstruction, nextInstruction);
         this.lastInstruction = interpolator(1);
@@ -66,29 +66,29 @@ const PieChart = () => {
     svg.selectAll('text')
       .data(data)
       .enter()
-      .append("text")
+      .append('text')
       .text((d) => `${d.name}: ${d.value}`) // label text
-      .style("fill", "#000")
-      .attr("transform", (data, index) => `translate(${(index * 100) + 42}, 20)`)
+      .style('fill', '#000')
+      .attr('transform', (data, index) => `translate(${(index * 100) + 42}, 20)`)
       .transition()
       .duration(700)
-      .style("font-size", "14px");
+      .style('font-size', '14px');
 
     svg.selectAll('rect')
       .data(data)
       .enter()
-      .append("rect")
+      .append('rect')
       .attr('width', 20)
       .attr('height', 15)
       .attr('stroke', '#000')
       .attr('stroke-width', 1)
-      .attr("fill", (d, i) => colorScale(i))
-      .attr("transform", (data, index) => `translate(${(index * 100) + 19}, 8)`)
+      .attr('fill', (d, i) => colorScale(i))
+      .attr('transform', (data, index) => `translate(${(index * 100) + 19}, 8)`)
 
   }, [data]);
 
   return (
-    <div style={{ marginBottom: "2rem", height: 1000 }}>
+    <div style={{ marginBottom: '2rem', height: 1000 }}>
       <svg width='500' height='400' ref={svgRef}></svg>
     </div>
   );
